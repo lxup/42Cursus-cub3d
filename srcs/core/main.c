eef2838a82879a220bae18d91ead61f32e8df973
Loup Quehec <lquehec@bess-f3r5s9.clusters.42paris.fr>
2024-03-22 18:25:43 +0100

    BORDEL IL EST 23H30 QUEST CE QUE JE FOUS A DEV CE TRUC ECLATE

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:11:13 by lquehec           #+#    #+#             */
-/*   Updated: 2024/03/22 18:12:13 by lquehec          ###   ########.fr       */
+/*   Updated: 2024/03/22 18:25:20 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	ft_exit_err(t_error error, char *msg)
 {
 	if (error)
-			ft_dprintf(2, C_RED "Error -> %s\n" C_RESET, msg);
+		ft_dprintf(2, C_RED "Error -> %s\n" C_RESET, msg);
 	return (exit(error), error);
 }
 
@@ -25,7 +25,8 @@ int	main(int ac, char **av)
 
 	errno = 0;
 	if (ac < 2)
-		return (ft_exit_err(ERR_ARGS, "U need to specify a map file, dumb ass !"));
+		return (ft_exit_err(ERR_ARGS, "U need to specify a map file, "\
+			"dumb ass !"));
 	else if (ac > 2)
 		return (ft_exit_err(ERR_ARGS, "Too many arguments, just think..."));
 	ft_init(&game, av[1]);
